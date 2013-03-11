e76e5e7f383a6d39698abf2aa1263fadadfab7d2
Frank Mertens <frank@cyblogic.de>
2013-03-11 19:18:38 +0100

    Fix for leap year: add leap day only to Feb.

--- DateLib.js
+++ DateLib.js
@@ -11,7 +11,7 @@ Date.daysInMonth = function(year, month) {
     return [
         31/*Jan*/, 28/*Feb*/, 31/*Mar*/, 30/*Apr*/, 31/*May*/, 30/*Jun*/,
         31/*Jul*/, 31/*Aug*/, 30/*Sep*/, 31/*Oct*/, 30/*Nov*/, 31/*Dec*/
-    ][month] + Date.leapYear(year)
+    ][month] + (month == 1) * Date.leapYear(year)
 }
 
 Date.weeksInMonth = function(year, month, weekday) {
