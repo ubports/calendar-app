fdcc7f6c0f833bd1d62c902d314ef7a7d7295164
Omer Akram <om26er@ubuntu.com>
2013-05-29 16:04:21 +0500

    fix more

--- tests/autopilot/ubuntu_calendar_app/emulators/main_window.py
+++ tests/autopilot/ubuntu_calendar_app/emulators/main_window.py
@@ -21,31 +21,33 @@ class MainWindow(object):
         return self.app.select_single("QQuickView")
 
     def get_panel(self):
-    	return self.app.select_single("Toolbar")
+        return self.app.select_single("Toolbar")
 
     def get_new_event_button(self):
-    	return self.app.select_many("Button", visible=True)[1]
+        return self.app.select_many("Button", visible=True)[1]
 
     def get_timeline_button(self):
-    	return self.app.select_many("Button", visible=True)[2]
+        return self.app.select_many("Button", visible=True)[2]
 
     def get_create_event_page(self):
-    	return self.app.select_single("NewEvent")
+        return self.app.select_single("NewEvent")
 
     def get_new_event_name_input_box(self):
-    	return self.app.select_single("TextField", objectName="newEventName")
+        return self.app.select_single("TextField", objectName="newEventName")
 
     def get_event_start_time_field(self):
-    	return self.app.select_single("TextField", objectName="startTimeInput")
+        return self.app.select_single("TextField", objectName="startTimeInput")
 
     def get_event_end_time_field(self):
-    	return self.app.select_single("TextField", objectName="endTimeInput")
+        return self.app.select_single("TextField", objectName="endTimeInput")
 
     def get_event_location_field(self):
-    	return self.app.select_single("TextField", objectName="eventLocationInput")
+        return self.app.select_single(
+            "TextField", objectName="eventLocationInput")
 
     def get_event_people_field(self):
-    	return self.app.select_single("TextField", objectName="eventPeopleInput")
+        return self.app.select_single(
+            "TextField", objectName="eventPeopleInput")
 
     def get_event_save_button(self):
-    	return self.app.select_single("Button", objectName="eventSaveButton")
+        return self.app.select_single("Button", objectName="eventSaveButton")
