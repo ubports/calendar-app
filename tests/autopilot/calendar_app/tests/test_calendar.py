e0c748c28913ab74ee13d45b83cdd4f8c71c4681
nskaggs <nicholas.skaggs@canonical.com>
2013-07-12 12:31:28 -0400

    add back newevent test, but commented for reference

--- tests/autopilot/calendar_app/tests/test_calendar.py
+++ tests/autopilot/calendar_app/tests/test_calendar.py
@@ -34,3 +34,53 @@ class TestMainWindow(CalendarTestCase):
         self.assertThat(event_view.eventViewType, Eventually(Equals("TimeLineView.qml")))
         self.ubuntusdk.click_toolbar_button("Diary")
         self.assertThat(event_view.eventViewType, Eventually(Equals("DiaryView.qml")))
+
+#commenting out for now until adding a new event works again
+    #def test_new_event(self):
+        ##click on new event button
+        #self.ubuntusdk.click_toolbar_button('New Event')
+
+        ##grab all the fields
+        ##event_view = self.main_window.get_event_view()
+        #event_name_field = self.main_window.get_new_event_name_input_box()
+        #start_time_field = self.main_window.get_event_start_time_field()
+        #end_time_field = self.main_window.get_event_end_time_field()
+        #location_field = self.main_window.get_event_location_field()
+        #people_field = self.main_window.get_event_people_field()
+        #save_button = self.main_window.get_event_save_button()
+
+        ##input a new event name
+        #eventTitle = "Test event" + str(time.time())
+
+        #self.pointing_device.click_object(event_name_field)
+        #self.keyboard.type(eventTitle)
+        #self.assertThat(event_name_field.text, Eventually(Equals(eventTitle)))
+
+        ##input start time
+        #self.pointing_device.click_object(start_time_field)
+        #self.keyboard.press_and_release("Ctrl+a")
+        #self.keyboard.type("11")
+        #self.assertThat(start_time_field.text, Eventually(Equals("11")))
+
+        ##input end time
+        #self.pointing_device.click_object(end_time_field)
+        #self.keyboard.press_and_release("Ctrl+a")
+        #self.keyboard.type("00")
+        #self.assertThat(end_time_field.text, Eventually(Equals("00")))
+
+        ##input location
+        #self.pointing_device.click_object(location_field)
+        #self.keyboard.type("My location")
+        #self.assertThat(location_field.text, Eventually(Equals("My location")))
+
+        ##input people
+        #self.pointing_device.click_object(people_field)
+        #self.keyboard.type("Me")
+        #self.assertThat(people_field.text, Eventually(Equals("Me")))
+
+        ##click save button
+        #self.pointing_device.click_object(save_button)
+
+        ##verify that the event has been created in timeline
+        #title_label = lambda: self.main_window.get_title_label(eventTitle)
+        #self.assertThat(title_label, Eventually(NotEquals(None)))
