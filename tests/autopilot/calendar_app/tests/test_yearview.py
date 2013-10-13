6d84ce0e63ed697be4c6bf7720205f2c368174a6
Andrea Cerisara <andreacerisara@gmail.com>
2013-10-13 14:34:08 +0200

    Added more tests for year view, refactoring.

--- tests/autopilot/calendar_app/tests/test_yearview.py
+++ tests/autopilot/calendar_app/tests/test_yearview.py
@@ -9,7 +9,7 @@
 Calendar app autopilot tests for the year view.
 """
 
-import datetime
+from datetime import datetime
 
 from autopilot.matchers import Eventually
 from testtools.matchers import Equals, NotEquals
@@ -25,53 +25,77 @@ class TestYearView(CalendarTestCase):
         self.main_view.switch_to_tab("yearTab")
         self.year_view = self.main_view.get_year_view()
 
-    # for year and month view the component indexed at 1
-    # is the one currently displayed.
-
     def test_selecting_a_month_switch_to_month_view(self):
         """It must be possible to select a month and open the month view."""
 
-        current_year_grid = self.year_view.select_many("QQuickFlickable")[1]
-        months = current_year_grid.select_many("MonthComponent")
-
+        months = self.months_from_selected_year()
         self.assert_current_year_is_default_one(months[0])
 
         february = months[1]
-        selected_month = self.get_month_name(february)
-        selected_year = self.get_year(february)
+        expected_month_name = self.get_month_name(february)
+        expected_year = self.get_year(february)
 
         self.pointing_device.click_object(february)
 
         month_view = self.main_view.get_month_view()
         self.assertThat(month_view.visible, Eventually(Equals(True)))
-        current_month = month_view.select_many("MonthComponent")[1]
+        selected_month = month_view.select_many("MonthComponent")[1]
 
-        self.assertThat(self.get_year(current_month), Equals(selected_year))
-        self.assertThat(self.get_month_name(current_month), Equals(selected_month))
+        self.assertThat(self.get_year(selected_month), Equals(expected_year))
+        self.assertThat(self.get_month_name(selected_month),
+                        Equals(expected_month_name))
 
     def test_current_day_is_selected(self):
         """The current day must be selected."""
 
-        current_year_grid = self.year_view.select_many("QQuickFlickable")[1]
-        months = current_year_grid.select_many("MonthComponent")
-        current_month = datetime.datetime.now().month
-        month = months[current_month - 1]   
-        month_grid = month.wait_select_single(objectName="monthGrid")
-        current_day = datetime.datetime.now().day
-        current_day_label = month_grid.wait_select_single("Label", text=str(current_day))        
+        months = self.months_from_selected_year()
+        current_month = months[datetime.now().month - 1]
+        month_grid = current_month.wait_select_single(objectName="monthGrid")
+
+        current_day_label = month_grid.wait_select_single(
+            "Label", text=str(datetime.now().day))
 
         # probably better to check the sorrounding UbuntuShape object,
-        # but get_parent() seems not implemented yet.
+        # upgrade this when python-autopilot 1.4 will be available.
         color = current_day_label.color
         label_color = (color[0], color[1], color[2], color[3])
-        self.assertThat(label_color, Equals((44, 0, 30, 255)))
+
+        self.assertThat(label_color, NotEquals((255, 255, 255, 255)))
+
+    def test_show_next_years(self):
+        """It must be possible to show next years by swiping the view."""
+        self.change_year(1)
+
+    def test_show_previous_years(self):
+        """It must be possible to show previous years by swiping the view."""
+        self.change_year(-1)
+
+    def change_year(self, direction, how_many=5):
+        current_year = datetime.now().year
+
+        for i in xrange(1, how_many):
+            self.main_view.swipe_view(direction, self.year_view, x_pad=0.15)
+            selected_year = datetime.fromtimestamp(
+                self.year_view.currentYear).year
+
+            self.assertThat(
+                selected_year, Equals(current_year + (i * direction)))
 
     def assert_current_year_is_default_one(self, month_component):
-        self.assertThat(self.get_year(month_component), Equals(datetime.datetime.now().year))
+        self.assertThat(self.get_year(month_component),
+                        Equals(datetime.now().year))
 
     def get_year(self, month_component):
-        return int(month_component.wait_select_single("Label", objectName="yearLabel").text)
+        return int(month_component.wait_select_single(
+            "Label", objectName="yearLabel").text)
 
     def get_month_name(self, month_component):
-        return month_component.wait_select_single("Label", objectName="monthLabel").text
+        return month_component.wait_select_single(
+            "Label", objectName="monthLabel").text
 
+    def months_from_selected_year(self):
+        # TODO: the component indexed at 1 is the one currently displayed,
+        # investigate a way to validate this assumption visually.
+
+        year_grid = self.year_view.select_many("QQuickFlickable")[1]
+        return year_grid.select_many("MonthComponent")
