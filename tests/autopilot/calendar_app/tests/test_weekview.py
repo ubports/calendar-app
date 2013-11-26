3482eeedb2bfaeee2ee7a22703a9ce42f72423b9
nskaggs <nicholas.skaggs@canonical.com>
2013-11-25 19:01:11 -0500

    fix locale sunday/monday issue and add logging

--- tests/autopilot/calendar_app/tests/test_weekview.py
+++ tests/autopilot/calendar_app/tests/test_weekview.py
@@ -32,6 +32,66 @@ class TestWeekView(CalendarTestCase):
 
         self.week_view = self.main_view.get_week_view()
 
+    def _change_week(self, direction):
+        #TODO: fix this locale issue. The lab needs a monday start date
+        #http://bugs.python.org/issue17659
+        #weekview has firstDate property we can use instead
+        #uses unix timestamp of first day of current week @ 5 am UTC
+
+        first_dow = self._get_first_day_of_week()
+
+        self.main_view.swipe_view(direction, self.week_view, x_pad=0.15)
+        day_start = self.week_view.dayStart.datetime
+
+        expected_day_start = first_dow + datetime.timedelta(
+            days=(7 * direction))
+
+        #replace hours / mins / seconds, just need to verify days
+        expected_day_start = expected_day_start.replace(
+            hour=0, minute=0, second=0, microsecond=0)
+
+        day_start = day_start.replace(
+            hour=0, minute=0, second=0, microsecond=0)
+
+        self.assertThat(day_start, Equals(expected_day_start))
+
+    def _get_days_of_week(self):
+        header = self.main_view.select_single(objectName="weekHeader")
+        timeline = header.select_many("TimeLineHeaderComponent")[0]
+        return sorted(timeline.select_many("Label", objectName="dateLabel"),
+                      key=lambda dateLabel: dateLabel.text)
+
+    def _get_first_day_of_week(self):
+        current_date = self.week_view.dayStart.datetime
+        firstDay = self.week_view.firstDay.datetime
+        if current_date != firstDay:
+            #support monday and sunday
+            #sunday
+            if firstDay.weekday() == 6:
+                weekday = current_date.weekday()
+                diff = datetime.timedelta(days=weekday + 1)
+                day_start = current_date - diff
+                logger.debug("Setting day_start %s, %s, %s" %
+                            (current_date, diff, weekday))
+            #saturday
+            elif firstDay.weekday() == 5:
+                weekday = current_date.weekday()
+                diff = datetime.timedelta(days=weekday + 1)
+                day_start = current_date - diff
+                logger.debug("Setting day_start %s, %s, %s" %
+                            (current_date, diff, weekday))
+            #monday
+            else:
+                weekday = current_date.weekday()
+                diff = datetime.timedelta(days=weekday)
+                day_start = current_date - diff
+                logger.debug("Setting day_start %s, %s, %s" %
+                            (current_date, diff, weekday))
+        else:
+            day_start = current_date
+            logger.debug("Using today as day_start %s" % current_date)
+        return day_start
+
     def test_current_month_and_year_is_selected(self):
         """By default, the week view shows the current month and year."""
 
@@ -50,22 +110,16 @@ class TestWeekView(CalendarTestCase):
         """By default, the week view shows the current week."""
 
         now = datetime.datetime.now()
-        days = self.get_days_of_week()
+        days = self._get_days_of_week()
 
-        #is today not sunday/monday? then nab sunday/monday
-        #TODO: fix this locale issue. The lab needs a monday start date
-        current_date = self.week_view.dayStart.datetime
-        if self.week_view.dayStart.datetime.weekday() != 6:
-            firstDay = current_date - (datetime.timedelta(
-                days=current_date.weekday()))
-        else:
-            firstDay = current_date
+        first_dow = self._get_first_day_of_week()
 
         for i in xrange(7):
             current_day = int(days[i].text)
-            expected_day = (firstDay + datetime.timedelta(days=i)).day
+            expected_day = (first_dow + datetime.timedelta(days=i)).day
 
-            logger.debug("Current %s, expected %s, first %s" % (current_day, expected_day, firstDay))
+            logger.debug("Current %s, expected %s, first %s" %
+                        (current_day, expected_day, first_dow))
 
             self.assertThat(current_day, Equals(expected_day))
             color = days[i].color
@@ -77,40 +131,9 @@ class TestWeekView(CalendarTestCase):
     def test_show_next_weeks(self):
         """It must be possible to show next weeks by swiping the view."""
         for i in xrange(6):
-            self.change_week(1)
+            self._change_week(1)
 
     def test_show_previous_weeks(self):
         """It must be possible to show previous weeks by swiping the view."""
         for i in xrange(6):
-            self.change_week(-1)
-
-    def change_week(self, direction):
-        #is today not sunday/monday? then nab sunday/monday
-        #TODO: fix this locale issue. The lab needs a monday start date
-        current_date = self.week_view.dayStart.datetime
-        if self.week_view.dayStart.datetime.weekday() != 6:
-            current_day_start = current_date - (datetime.timedelta(
-                days=current_date.weekday()))
-        else:
-            current_day_start = current_date
-
-        self.main_view.swipe_view(direction, self.week_view, x_pad=0.15)
-        day_start = self.week_view.dayStart.datetime
-
-        expected_day_start = current_day_start + datetime.timedelta(
-            days=(7 * direction))
-
-        #replace hours / mins / seconds, just need to verify days
-        expected_day_start = expected_day_start.replace(
-            hour=0, minute=0, second=0, microsecond=0)
-
-        day_start = day_start.replace(
-            hour=0, minute=0, second=0, microsecond=0)
-
-        self.assertThat(day_start, Equals(expected_day_start))
-
-    def get_days_of_week(self):
-        header = self.main_view.select_single(objectName="weekHeader")
-        timeline = header.select_many("TimeLineHeaderComponent")[0]
-        return sorted(timeline.select_many("Label", objectName="dateLabel"),
-                      key=lambda dateLabel: dateLabel.text)
+            self._change_week(-1)
