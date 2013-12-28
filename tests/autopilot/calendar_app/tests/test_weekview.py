02284f8ebf2d745071f34e37547ae3fb3d7c7288
Kunal Parmar <pkunal.parmar@gmail.com>
2013-12-28 14:01:45 +0900

    weekview autopilot test fix

--- tests/autopilot/calendar_app/tests/test_weekview.py
+++ tests/autopilot/calendar_app/tests/test_weekview.py
@@ -70,7 +70,7 @@ class TestWeekView(CalendarTestCase):
 
     def _get_date_label_headers(self):
         header = self.main_view.select_single(objectName="weekHeader")
-        timeline = header.select_many("TimeLineHeaderComponent")[0]
+        timeline = header.select_many("TimeLineHeaderComponent")[1]
         dateLabels = timeline.select_many("Label", objectName="dateLabel")
         return dateLabels
 
