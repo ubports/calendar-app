4d50c803c74125231ed68d62d9db1a97da9a80d5
Kunal Parmar <pkunal.parmar@gmail.com>
2014-12-19 06:02:02 +0900

    reducing swipe size

--- tests/autopilot/calendar_app/__init__.py
+++ tests/autopilot/calendar_app/__init__.py
@@ -214,7 +214,7 @@ class MainView(ubuntuuitoolkit.MainView):
                 sleep(1)
             timeout += 1
 
-    def swipe_view(self, direction, view, x_pad=0.15):
+    def swipe_view(self, direction, view, x_pad=0.20):
         """Swipe the given view to left or right.
 
         Args:
