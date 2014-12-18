7b6183db8df56ea9e339bf8eca6da537f8bf0aa4
Kunal Parmar <pkunal.parmar@gmail.com>
2014-12-19 00:59:19 +0900

    AP fix

--- tests/autopilot/calendar_app/__init__.py
+++ tests/autopilot/calendar_app/__init__.py
@@ -399,7 +399,10 @@ class DayView(ubuntuuitoolkit.UbuntuUIToolkitCustomProxyObjectBase):
 
         for event in event_bubbles:
             # Event-bubbles objects are recycled, only show visible ones.
-            if event.get_name() == event_name:
+            temp = "<b>"+event_name+"</b>"
+            print("#######################")
+            print(event.get_name() , "-----" , temp)
+            if event.get_name() == temp:
                 if (visible and event.visible) or not visible:
                     matched_event = event
                     return matched_event
