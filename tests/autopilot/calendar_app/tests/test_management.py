55ce0551aee2d83580b18649843368125d42cf6e
nskaggs <nicholas.skaggs@canonical.com>
2016-03-03 11:43:54 -0500

    removed more tests that only require single page in favor of qmltests

--- tests/autopilot/calendar_app/tests/test_management.py
+++ tests/autopilot/calendar_app/tests/test_management.py
@@ -29,22 +29,6 @@ from calendar_app.tests import CalendarAppTestCaseWithVcard
 
 class TestManagement(CalendarAppTestCaseWithVcard):
 
-    def test_change_calendar_color(self):
-        """ Test changing calendar color   """
-        calendar_choice_popup = \
-            self.app.main_view.go_to_calendar_choice_popup()
-        original_calendar_color = \
-            calendar_choice_popup.get_calendar_color()
-        calendar_choice_popup.open_color_picker_dialog()
-        colorPickerDialog = self.app.main_view.get_color_picker_dialog()
-        colorPickerDialog.change_calendar_color("color6")
-
-        final_calendar_color = \
-            calendar_choice_popup.get_calendar_color()
-
-        self.assertThat(
-            original_calendar_color, NotEquals(final_calendar_color))
-
     def test_unselect_calendar(self):
         """ Test unselecting calendar
 
