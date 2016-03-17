2f376f938500a4ced873f89df40cee905e658e6e
Arthur Mello <arthur.mello@canonical.com>
2016-03-17 15:28:07 -0300

    Move package dir into build dir

--- click-build.sh
+++ click-build.sh
@@ -11,6 +11,5 @@ cd $CLICKARCH-build
 cmake .. -DCLICK_MODE=on \
         -DBZR_REVNO=$(cd ..; bzr revno) \
         -DBZR_SOURCE="$BZR_SOURCE"
-make DESTDIR=../package install
-cd ..
+make DESTDIR=./package install
 click build package
